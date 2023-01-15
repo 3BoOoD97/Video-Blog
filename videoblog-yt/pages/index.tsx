18c4542bfeb812021ed435e2c12c1c19cc979922
3BoOoD97 <abodn70@hotmail.com>
2023-01-15 23:25:20 +0100

    Initialize the project

---
+++ videoblog-yt/pages/index.tsx
@@ -0,0 +1,18 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-mainBg w-screen h-screen">
+      <Head>
+        <title>Video Blog - Next Js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    <h1>Hey Guys....</h1>
+    </div>
+  )
+}
+
+export default Home
