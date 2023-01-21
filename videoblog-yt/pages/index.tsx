1568791da834b290212801097b0f562c1a9fda5f
3BoOoD97 <abodn70@hotmail.com>
2023-01-21 19:00:31 +0100

    MainContainer Done

--- videoblog-yt/pages/index.tsx
+++ videoblog-yt/pages/index.tsx
@@ -6,13 +6,15 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
 
-    <div className="bg-mainBg w-screen h-screen">
+    <div className="bg-mainBg w-screen h-screen overflow-hidden flex flex-col">
       <Head>
         <title>Video Blog - Next Js</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-    <Header/>
+      <Header/>
+    {/* Main Container */}
+    
+    <div className='bg-blue-400 w-full h-[calc(100%-80px)]'> </div>
     </div>
   )
 }
